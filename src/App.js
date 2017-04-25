5888023b417e97643fe261bb00af94b9c757b113
Jen J Wong <jenjwong@gmail.com>
2017-04-25 15:59:23 -0700

    adds debouncing

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import SearchBar from './SearchBar';
 import YTSearch from 'youtube-api-search';
 import VideoList from './VideoList';
 import VideoDetail from './VideoDetail';
+import {debounce} from 'lodash';
 
 
 import './App.css';
@@ -12,31 +13,31 @@ class App extends Component {
   constructor(props){
     super(props)
     this.state = {
-      searchTerm: 'brie',
+      searchTerm: '',
       videos: [],
       selectedVideo: ''
     }
     this._onVideoSelect = this._onVideoSelect.bind(this);
+    this._onVideoSearch = debounce(this._onVideoSearch.bind(this), 100);
   }
 
   componentDidMount() {
-    this._getVideos()
+    this._onVideoSearch('brie')
   }
 
-  _getVideos() {
-    YTSearch({key: 'AIzaSyDC6TUz6AdJTEkZEmtfLs7X_4MBvT8rH9E', term: this.state.searchTerm}, (videos) => { this.setState({videos, selectedVideo: videos[0]});
+  _onVideoSearch(searchTerm) {
+    YTSearch({key: 'AIzaSyDC6TUz6AdJTEkZEmtfLs7X_4MBvT8rH9E', term: searchTerm}, (videos) => { this.setState({videos, selectedVideo: videos[0]});
     });
   }
 
   _onVideoSelect(video) {
-    console.log(video, 'video')
     this.setState({selectedVideo: video});
   }
 
   render() {
     return (
       <div className="App">
-        <SearchBar />
+        <SearchBar _onVideoSearch={this._onVideoSearch}/>
         <VideoList videos={this.state.videos} _onVideoSelect={this._onVideoSelect} />
         <VideoDetail video={this.state.selectedVideo} />
       </div>
--- src/SearchBar.js
+++ src/SearchBar.js
@@ -17,16 +17,17 @@ export default class SearchBar extends PureComponent {
     this.setState({
       text: e.target.value
     })
-  }
-
-  _handleSubmit() {
+    this.props._onVideoSearch(this.state.text);
   }
 
   render() {
     return (
       <div>
         {this.state.text}
-        <input type="text" onChange={this._handleInputChange.bind(this)} value={this.state.text} placeholder="Search You Tube"/>
+        <form action={this._handleSubmit}>
+          <input type="text" onChange={this._handleInputChange.bind(this)} value={this.state.text} placeholder="Search You Tube"/>
+          <button>Search</button>
+        </form>
       </div>
     )
   }
